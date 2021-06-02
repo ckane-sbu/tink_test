44803d238942ee8f5cd799d3c9bd71f4c5d7a785
ckane <christopher.kane@stonybrook.edu>
2021-06-01 23:02:08 -0400

    Second commit

--- cleartext_keyset.py
+++ cleartext_keyset.py
@@ -1,29 +1,46 @@
+
+
 from absl import app, flags, logging
 import tink
 from tink import aead, cleartext_keyset_handle
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_enum('mode', None, ['encrypt', 'decrypt'],
+flags.DEFINE_enum('mode', None, ['generate', 'encrypt', 'decrypt'],
                   'The operation to perform.')
 flags.DEFINE_string('keyset_path', None,
-                    'Path to the keyset used for encryption.')
+                    'Path to the keyset used for the encryption.')
 flags.DEFINE_string('input_path', None, 'Path to the input file.')
 flags.DEFINE_string('output_path', None, 'Path to the output file.')
-flags.DEFINE_string('associated_data', None,
-                    'Optional associated data used for the encryption.')
 
 def main(argv):
-    del argv # Unused
+    del argv
 
-    associated_data = b'' if not FLAGS.associated_data else bytes(FLAGS.associated_data, 'utf-8')
-
-    #Initialize Tink
+    # Initialize Tink
     try:
         aead.register()
     except tink.TinkError as e:
         logging.error('Error initializing Tink: %s', e)
         return 1
+
+    if FLAGS.mode == 'generate':
+        # Generate a new keyset
+        try:
+            key_template = aead.aead_key_templates.AES128_GCM
+            keyset_handle = tink.KeysetHandle.generate_new(key_template)
+        except tink.TinkError as e:
+            logging.exception('Error creating primitive: %s', e)
+            return 1
+
+        with open(FLAGS.keyset_path, 'wt') as keyset_file:
+            try:
+                cleartext_keyset_handle.write(tink.JsonKeysetWriter(keyset_file), keyset_handle)
+            except tink.TinkError as e:
+                logging.exception('Error writing key: %s', e)
+                return 1
+        return 0
+
+    # Use the input keyset to encrypt/decrypt data
 
     # Read the keyset into a keyset_handle
     with open(FLAGS.keyset_path, 'rt') as keyset_file:
@@ -38,24 +55,22 @@ def main(argv):
     try:
         cipher = keyset_handle.primitive(aead.Aead)
     except tink.TinkError as e:
-        logging.error('Error creating primitives: %s', e)
+        logging.error('Error creating primitive: %s', e)
         return 1
 
     with open(FLAGS.input_path, 'rb') as input_file:
         input_data = input_file.read()
         if FLAGS.mode == 'decrypt':
-            output_data = cipher.decrypt(input_data, associated_data)
+            output_data = cipher.decrypt(input_data, b'envelope_example')
         elif FLAGS.mode == 'encrypt':
-            output_data = cipher.encrypt(input_data, associated_data)
+            output_data = cipher.encrypt(input_data, b'envelope_example')
         else:
-            logging.error(
-                'Error mode not supported. Please choose "encrypt" or "decrypt".')
+            logging.error('Error mode not supported. Please choose "encrypt" or "decrypt".')
             return 1
 
         with open(FLAGS.output_path, 'wb') as output_file:
             output_file.write(output_data)
 
 if __name__ == "__main__":
-    flags.mark_flags_as_required([
-        'mode', 'keyset_path', 'input_path', 'output_path'])
+    flags.mark_flags_as_required(['mode', 'keyset_path'])
     app.run(main)
